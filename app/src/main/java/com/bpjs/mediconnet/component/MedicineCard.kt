4423907a29a4c66bcf59a03ab180ccc6563db564
tsukifell <calvinsaputra217@gmail.com>
2023-09-09 11:06:26 +0700

    feat(MedicineCard and CategoryChip): Menambahkan komponen MedicineCard dan CategoryChip

---
+++ app/src/main/java/com/bpjs/mediconnet/component/MedicineCard.kt
@@ -0,0 +1,103 @@
+package com.bpjs.mediconnet.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.bpjs.mediconnet.R
+
+@Composable
+fun MedicineCard(
+    name: String,
+    description: String,
+    imageUrl: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .padding(16.dp)
+            .size(width = 240.dp, height = 300.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        )
+    ) {
+        Row {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = stringResource(R.string.pharmacy_image),
+                modifier = Modifier
+                    .size(width = 240.dp, height = 160.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.FillBounds
+            )
+        }
+
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row {
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontSize = 16.sp
+                    )
+                )
+
+                Icon(
+                    imageVector = Icons.Default.FavoriteBorder,
+                    contentDescription = stringResource(R.string.favorite),
+                    modifier = Modifier.size(24.dp),
+                    tint = Color.Red
+                )
+
+            }
+
+            Divider(
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+            Text(
+                text = description,
+                textAlign = TextAlign.Justify,
+                lineHeight = 20.sp,
+                maxLines = 8,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMedicineCard() {
+    MedicineCard(
+        name = "Paracetamol",
+        description = "Paracetamol diketahui dapat bekerja pada pusat pengaturan suhu yang ada di otak untuk menurunkan suhu tubuh saat seseorang sedang mengalami demam. Selain itu, paracetamol juga bisa menghambat pembentukan prostaglandin, yaitu senyawa yang memicu nyeri dan bengkak ketika terjadi kerusakan atau cedera pada jaringan tubuh.",
+        imageUrl = "https://o-cdf.sirclocdn.com/unsafe/cdnapi.sooplai.com/media/products/201993/MEDI-KFAR-003A-1.jpg"
+    )
+}

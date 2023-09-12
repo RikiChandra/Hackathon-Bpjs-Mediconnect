63d66a7c2512bb2df931a2d2151ce93b162de890
JosuaMnl <josuamnalu@gmail.com>
2023-09-12 23:50:20 +0700

    chore: Add Dagger-Hilt depedency and change JVM version to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ import com.android.build.api.variant.BuildConfigField
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -32,11 +34,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -87,4 +89,9 @@ dependencies {
     //Animation
     implementation("androidx.compose.animation:animation:1.0.0")
 
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.47")
+    kapt("com.google.dagger:hilt-android-compiler:2.47")
+
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 }

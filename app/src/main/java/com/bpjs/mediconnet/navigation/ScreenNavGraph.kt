dfca14f883ee63d80f26e44d63ae04aa3958f7ea
JosuaMnl <josuamnalu@gmail.com>
2023-09-16 00:33:42 +0700

    feat: Add NavGraph for Detail Medicine

--- app/src/main/java/com/bpjs/mediconnet/navigation/ScreenNavGraph.kt
+++ app/src/main/java/com/bpjs/mediconnet/navigation/ScreenNavGraph.kt
@@ -1,11 +1,15 @@
 package com.bpjs.mediconnet.navigation
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.bpjs.mediconnet.screen.FeedbackScreen
+import com.bpjs.mediconnet.screen.MedicineDetailScreen
 import com.bpjs.mediconnet.screen.MedicineScreen
 import com.bpjs.mediconnet.screen.PharmacyScreen
 
@@ -28,7 +32,19 @@ fun ScreenNavGraph(
         }
 
         composable(route = BottomNavScreen.Medicine.route) {
-            MedicineScreen()
+            MedicineScreen(
+                navigateToDetail = { medicineId ->
+                    navController.navigate(Screen.DetailMedicine.createRoute(medicineId))
+                }
+            )
+        }
+
+        composable(
+            route = Screen.DetailMedicine.route,
+            arguments = listOf(navArgument("medicineId") { type = NavType.StringType })
+        ) {
+            val medicineId = it.arguments?.getString("medicineId") ?: ""
+            MedicineDetailScreen(medicineId = medicineId, navController = navController)
         }
 
         composable(route = BottomNavScreen.Feedback.route) {

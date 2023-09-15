6395f65603c0a1127ecf109521785fbc4c43fe9f
JosuaMnl <josuamnalu@gmail.com>
2023-09-16 00:35:44 +0700

    feat: Add condition for don't show bottom bar on the Detail Medicine Screen

--- app/src/main/java/com/bpjs/mediconnet/MediConnectScreen.kt
+++ app/src/main/java/com/bpjs/mediconnet/MediConnectScreen.kt
@@ -4,22 +4,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.bpjs.mediconnet.elements.BottomNav
+import com.bpjs.mediconnet.navigation.Screen
 import com.bpjs.mediconnet.navigation.ScreenNavGraph
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MediConnectScreen(navController: NavHostController = rememberNavController()) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
     Scaffold(
         bottomBar = {
-            BottomNav(navController = navController)
+            if (currentRoute != Screen.DetailMedicine.route) BottomNav(navController = navController)
         }
-    ){ innerPadding ->
+    ) { innerPadding ->
         ScreenNavGraph(
             modifier = Modifier.padding(innerPadding),
-            navController = navController)
+            navController = navController
+        )
     }
 }

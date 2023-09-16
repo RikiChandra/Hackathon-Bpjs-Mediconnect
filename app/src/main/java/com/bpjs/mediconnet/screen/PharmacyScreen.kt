b964865f7d500aa2d95d9c4e1aaa1d7a11b99bdb
RikiChandra <rikichandra37@gmail.com>
2023-09-16 17:02:15 +0700

    feat : Update code PharmacyScreen

--- app/src/main/java/com/bpjs/mediconnet/screen/PharmacyScreen.kt
+++ app/src/main/java/com/bpjs/mediconnet/screen/PharmacyScreen.kt
@@ -4,20 +4,22 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.bpjs.mediconnet.component.PharmacyCard
-import com.bpjs.mediconnet.component.ShimmerPharmacyItem
 import com.bpjs.mediconnet.elements.HeaderScreen
 import com.bpjs.mediconnet.helper.UiState
 import com.bpjs.mediconnet.model.DataItem
@@ -41,13 +43,13 @@ fun PharmacyScreen(
         viewModel.uiState.collectAsState(initial = UiState.Loading).value.let { uiState ->
             when (uiState) {
                 is UiState.Loading -> {
-                    LazyColumn {
-                        items(10) {
-                            ShimmerPharmacyItem()
-                            Spacer(modifier = Modifier.height(16.dp))
-                        }
-                    }
                     viewModel.getPharmacies()
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
                 }
                 is UiState.Success -> {
                     PharmacyContent(pharmacies = uiState.data, navigateToDetail = navigateToDetail)

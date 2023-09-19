ba5c88c05ce7c5e76e63e50c643509654aa6df4a
tsukifell <calvinsaputra217@gmail.com>
2023-09-19 15:56:17 +0700

    refactor: Refactoring the HomeScreen layout

--- app/src/main/java/com/bpjs/mediconnet/component/CategoryChips.kt
+++ app/src/main/java/com/bpjs/mediconnet/component/CategoryChips.kt
@@ -1,7 +1,7 @@
 package com.bpjs.mediconnet.component
 
-import androidx.compose.material3.AssistChip
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
@@ -9,7 +9,12 @@ import androidx.compose.runtime.Composable
 @Composable
 fun CategoryChips(
     labelName: String,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
-    AssistChip(onClick = onClick, label = { Text(labelName) })
+    SuggestionChip(
+        onClick = onClick, label = {
+            Text(labelName)
+        }
+    )
 }
+
